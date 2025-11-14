df79a8081d7023d878a1cb738bb521ed6b2ce301
aymench19 <mnchairat@gmail.com>
2025-11-14 09:32:58 +0100

    Initial commit

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -1,4 +1,5 @@
-<?php  
+<?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -7,37 +8,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class EtudiantController extends AbstractController
 {
-    #[Route('/etudiant', name: 'etudiant')]
-    public function index(): Response
+    #[Route('/list', name: 'list_etudiant')]
+    public function listEtudiant(): Response
     {
-        return new Response("Bonjour mes étudiants");
+        // Tableau simple
+        $etudiants = ['Ali', 'Sarra', 'Ahmed'];
+
+        // Tableau associatif
+        $modules = [
+            ["id" => 1 ,"enseignant" => "Ali" ,"nbr_heures" => 30 , "date_creation" => "2023-01-15" ],
+            ["id" => 2 ,"enseignant" => "Sarra" ,"nbr_heures" => 25 , "date_creation" => "2023-01-16" ],
+            ["id" => 3 ,"enseignant" => "Ahmed" ,"nbr_heures" => 20 , "date_creation" => "2023-01-17" ],
+        ];
+
+return $this->render('etudiant/list.html.twig', [
+    'etudiants' => $etudiants,
+    'modules' => $modules,
+]);
+
     }
-    #[Route('/etudiant/{id}', name: 'affichage_etudiant', requirements: ['id' => '\d{2}'])]
-    public function affichageEtudiant($id): Response
+
+    #[Route('/affecter', name: 'affecter_etudiant')]
+    public function affecter(): Response
     {
-        return new Response("Bonjour étudiant numéro : " . $id);
+        return $this->render('etudiant/affecter.html.twig');
     }
-    
 
-    #[Route('/etudiant/{name}', name: 'etudiant_name')]
-    public function voirNom(string $name): Response
-   {
-    return $this->render('etudiant/etudiant.html.twig', [
-        'name' => $name,
-    ]);
-   }
-   #[Route('/list', name: 'liste')]
-    public function listEtudiant()
-   {
-    $etudiants=array("aymen","ali");
-    
-    $modules= array(
-        array("nom"=>"symfony","id"=>1,"enseignant"=>"mohamed","nbre_heures"=>42,"date"=>"12-12-2021"),
-        array("nom"=>"JEE","id"=>2,"enseignant"=>"salah","nbre_heures"=>31,"date"=>"12-10-2021"),
-        array("nom"=>"BD","id"=>3,"enseignant"=>"Islem","nbre_heures"=>21,"date"=>"12-09-2021"),);
+    #[Route('/', name: 'index_fils')]
+    public function indexFils(): Response
+    {
+$etudiants = ['Ali', 'Sarra', 'Ahmed'];
+    $modules = [
+        ["id" => 1 ,"enseignant" => "Ali" ,"nbr_heures" => 30 , "date_creation" => "2023-01-15" ],
+        ["id" => 2 ,"enseignant" => "Sarra" ,"nbr_heures" => 25 , "date_creation" => "2023-01-16" ],
+        ["id" => 3 ,"enseignant" => "Ahmed" ,"nbr_heures" => 20 , "date_creation" => "2023-01-17" ],
+    ];
 
-    return $this->render('etudiant/list.html.twig',
-        array ('etudiants' => $etudiants, 'listemodules' => $modules));
-   }
-
+    return $this->render('etudiant/index.html.twig', [
+        'etudiants' => $etudiants,
+        'modules' => $modules,
+    ]);    }
 }
